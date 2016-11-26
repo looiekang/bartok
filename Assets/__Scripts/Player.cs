0d3b4da8323b564b2816c2321138271ebad23fbf
looiekang <louiskang@Louiss-MBP.hsd1.nj.comcast.net>
2016-11-26 18:09:20 -0500

    Additions to Bartok and Player

--- Assets/__Scripts/Player.cs
+++ Assets/__Scripts/Player.cs
@@ -90,10 +90,15 @@ public class Player {
 			//  but which does keep their colliders from overlapping
 			pos.z = -0.5f*i;
 
-			// Set the localPosition and rotation of the ith card in the hand
-			hand[i].transform.localPosition = pos;
-			hand[i].transform.rotation = rotQ;
-			hand[i].state = CBState.hand;
+			//	Set	the	localPosition	and	rotation	of	the	ith	card	in	the hand
+			hand[i].MoveTo(pos, rotQ);  //	Tell	CardBartok	to	interpolate			
+			hand[i].state	=	CBState.toHand;									
+			//	^	After	the	move,	CardBartok	will	set	the	state	to CBState.hand
+
+
+			// hand[i].transform.localPosition	=	pos;
+			// hand[i].transform.rotation	=	rotQ;
+			// hand[i].state	=	CBState.hand;
 
 			// This uses a comparison operator to return a true or false bool
 			// So, if (type == PlayerType.human), hand[i].faceUp is set to true
